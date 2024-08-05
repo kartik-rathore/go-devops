fa27c1d61826ae5a67c4743f7a0c3f35da344b66
Kartik Singh Rathore <166915596+kartik-rathore@users.noreply.github.com>
2024-08-06 00:01:03 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,17 +5,25 @@ import (
     "net/http"
 )
 
-// Parse the template file
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
-// Handler function to render the HTML page
 func handler(w http.ResponseWriter, r *http.Request) {
     templates.ExecuteTemplate(w, "index.html", nil)
 }
 
-// Main function to set up the server
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        name := r.FormValue("name")
+        greeting := "Hi, " + name + "! Nice to meet you!"
+        w.Write([]byte(greeting))
+    } else {
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    }
+}
+
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/greet", greetHandler)
     println("Server started at http://localhost:8080")
     if err := http.ListenAndServe(":8080", nil); err != nil {
         println("Error starting server:", err)
